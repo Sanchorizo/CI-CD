16e72c9fe8feeb2839551ef8e6af7fb36e4c1681
Sanchorizo <maxime--martinez@hotmail.fr>
2024-06-17 15:18:40 +0200

    second commit

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prenom VARCHAR(50),
+    nom VARCHAR(50)
+);
+
+INSERT INTO employees (first_name, last_name) VALUES ('Jo', 'Lopez');
+INSERT INTO employees (first_name, last_name) VALUES ('David', 'Lopez');
