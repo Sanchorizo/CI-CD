662ea2ca871f6a3d6a2be63208fe8b0a6113e10b
Sanchorizo <maxime--martinez@hotmail.fr>
2024-06-17 15:31:04 +0200

    supress insert

--- database.sql
+++ database.sql
@@ -3,6 +3,3 @@ CREATE TABLE employees (
     prenom VARCHAR(50),
     nom VARCHAR(50)
 );
-
-INSERT INTO employees (first_name, last_name) VALUES ('Jo', 'Lopez');
-INSERT INTO employees (first_name, last_name) VALUES ('David', 'Lopez');
